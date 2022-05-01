7a65c8975babd5b33ed6c942969b51d3f4ba2cd5
Daniel López Sánchez <dlopezs2009@hotmail.com>
2022-05-01 12:54:50 -0500

    clear data

--- src/components/content/ContentApp.jsx
+++ src/components/content/ContentApp.jsx
@@ -1,29 +1,18 @@
 import React, { Fragment, useState, useRef, useEffect } from "react";
-import { Tabs, PageHeader, Table, Input, Row, Col, Select } from 'antd';
+import { Tabs, PageHeader, Table, Input, Row, Col, Select, Radio, Space, Button } from 'antd';
+import { PlusOutlined, DownloadOutlined, UploadOutlined } from '@ant-design/icons';
 import * as API from '../../services/api';
 
 const { TabPane } = Tabs;
 const { Option } = Select;
 
 export function ContentApp() {
-    const data = [];
-    for (let i = 0; i < 100; i++) {
-        data.push({
-            key: i,
-            division: `John Brown ${i}`,
-            superior_division: `John Brown ${i}`,
-            collaborators: 32,
-            level: 3,
-            subdivisions: 3,
-            ambassadors: `New York No. 1 Lake Park ${i}`,
-        });
-    }
     const columns = [
             {
                 title: 'División',
-                dataIndex: 'division',
-                key: 'division',
-                sorter: (a, b) => a.division - b.division,
+                dataIndex: 'name',
+                key: 'name',
+                sorter: (a, b) => a.name - b.name,
                 filters: [
                     {
                         text: 'Joe',
@@ -67,9 +56,9 @@ export function ContentApp() {
             },
             {
                 title: 'Colaboradores',
-                dataIndex: 'collaborators',
-                key: 'collaborators',
-                sorter: (a, b) => a.collaborators - b.collaborators,
+                dataIndex: 'number_collaborators',
+                key: 'number_collaborators',
+                sorter: (a, b) => a.number_collaborators - b.number_collaborators,
             },
             {
                 title: 'Nivel',
@@ -102,49 +91,75 @@ export function ContentApp() {
             },
             {
                 title: 'Embajadores',
-                dataIndex: 'ambassadors',
-                key: 'ambassadors',
+                dataIndex: 'ambassador',
+                key: 'ambassador',
             },
         ];
 
-    const [dataSource, setDataSource] = useState(data);
+    const { selectedRowKeys } = useState();
+    const rowSelection = {
+      selectedRowKeys,
+    };
+
+    const [dataSource, setDataSource] = useState([]);
     const searchInputRef = useRef();
+    const [searchColumn, setSearchColumn] = useState('name');
+
+    useEffect( () => {
+        API.getAllDivisions().then(setDataSource);
+    }, [])
 
     const handleSearch = () => {
         const search = searchInputRef.current.input.value;
-        if (search == '') {
-            setDataSource(dataSource);
-            
+        
+        if (search === '') {
+            clearDataFiletered()
             return;
         }
 
         const filteredData = dataSource.filter(entry =>
-            entry.division.includes(search)
+            entry[searchColumn].includes(search)
         );
 
         setDataSource(filteredData);
+    }
+
+    const clearDataFiletered = () => {
+        API.getAllDivisions().then(setDataSource);
     }
 
     return (
         <PageHeader
             className="site-page-header-responsive"
             title="Organización"
+            extra={[
+                <Button key="3" type="primary" icon={<PlusOutlined />} />,
+                <Button key="2" icon={<DownloadOutlined />} />,
+                <Button key="1" icon={<UploadOutlined />} />,
+              ]}
             footer={
                 <Tabs defaultActiveKey="1">
                     <TabPane tab="Divisiones" key="1">
-                        <Row>
-                            <Col span={8}></Col>
+                        <Row style={{ marginBottom: 24, marginTop: 24 }}>
+                            <Col span={8}>
+                                <Radio.Group defaultValue="list">
+                                    <Radio.Button value="list">Listado</Radio.Button>
+                                    <Radio.Button value="tree">Arbol</Radio.Button>
+                                </Radio.Group>
+                            </Col>
                             <Col span={8} offset={8}>
-                                <Select defaultValue="division" style={{ width: 200 }}>
-                                    <Option value="division">División</Option>
-                                    <Option value="superior_division">División Superior</Option>
-                                    <Option value="ambassadors">Embajador</Option>
-                                </Select>
-                                <Input style={{ width: 300 }} placeholder="Buscar..." ref={searchInputRef} onKeyUp={handleSearch} />
+                                <Space>
+                                    <Select onChange={value => { setSearchColumn(value); }}  defaultValue="name" style={{ width: 200 }}>
+                                        <Option value="name">División</Option>
+                                        <Option value="superior_division">División Superior</Option>
+                                        <Option value="ambassador">Embajador</Option>
+                                    </Select>
+                                    <Input style={{ width: 300 }} placeholder="Buscar..." allowClear ref={searchInputRef} onKeyUp={handleSearch} />
+                                </Space>
                             </Col>
                         </Row>
                         
-                        <Table columns={columns} dataSource={dataSource}  showSizeChanger pagination={{ position: ['none', 'bottomRight'] }} />
+                        <Table rowSelection={rowSelection} columns={columns} dataSource={dataSource}  showSizeChanger pagination={{ position: ['none', 'bottomRight'] }} />
                     </TabPane>
                     <TabPane tab="Colaboradores" key="2">
                         Colaboradores Tap
--- src/services/api.js
+++ src/services/api.js
@@ -4,10 +4,9 @@ export async function getAllDivisions() {
     try {
         const response = await fetch(`${API_URL}/divisions/get-divisions`);
         const data = await response.json()
-
-        return data;
+        
+        return data.data;
     } catch (error) {
         console.log(error)
     }
-    
 }
